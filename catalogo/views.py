027721a1b87d203cf53aaf4f0e3cfc5170815c29
saulo-alonso-de-souza-pereira <lesteamericano85@gmail.com>
2025-07-18 17:03:31 -0300

    Fix: Authorization

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Produto, Entrega
 from .serializers import ProdutoSerializer, EntregaSerializer
 from .permissions import IsAdminUser, IsEntregadorUser, IsComumUser, IsOwnerOrAdminReadOnly
